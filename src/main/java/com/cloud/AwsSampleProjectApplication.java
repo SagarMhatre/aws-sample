a6d7b024170208d4078d00dcb3cd4a23d7f4d3c2
Sagar Mhatre <sagarmmhatre@yahoo.co.in>
2017-12-23 06:47:35 +0530

    Initial Draft
    Started with a Spring Boot Starter Project

---
+++ src/main/java/com/cloud/AwsSampleProjectApplication.java
@@ -0,0 +1,12 @@
+package com.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsSampleProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsSampleProjectApplication.class, args);
+	}
+}
